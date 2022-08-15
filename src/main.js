4b355e9fbc7b622cde34e125179d1aeb4a41bd2d
Løøt <66328711+L-o-o-t@users.noreply.github.com>
2022-08-15 11:29:37 -0400

    additional setup for TailWinds I forgot is done.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/css/main.css'
-import './assets/css/styles.css'
+import './input.css'
 
 const app = createApp(App)
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
