a9c8c63b50bdb68c91e619c8bfd20407d8f73605
Løøt <66328711+L-o-o-t@users.noreply.github.com>
2022-08-17 11:03:51 -0400

    Add Fonts extend more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      title: ["Silkscreen", "cursive"],
+      body: ["Anton", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        backgroud: "#292c35", // index backgroud
+        "box-bg": "#353944",
+        "box-bg-dark": "#2a2c32",
+        border: "#404658",
+        "dot-line": "#404552",
+        "icon-color": "#4c535f",
+        primary: "#472ba2",
+        "primary-light": "#5533c2",
+        secondary: "#d1ec1a",
+      },
+    },
   },
   plugins: [],
-}
+};
