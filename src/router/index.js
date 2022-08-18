5f80ceb67d0ba3b77981bea4dfce5fe02fb9f3e3
Løøt <66328711+L-o-o-t@users.noreply.github.com>
2022-08-18 16:27:09 -0400

    Refactor SupportView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DashboardView from "../views/DashboardView.vue";
+import SupportView from "../views/SupportView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +11,9 @@ const router = createRouter({
       component: DashboardView,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/support",
+      name: "support",
+      component: SupportView,
     },
   ],
 });
