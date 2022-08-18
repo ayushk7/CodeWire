1958bc8e604595945b245ae933fa4b9e760e464f
Løøt <66328711+L-o-o-t@users.noreply.github.com>
2022-08-18 12:50:55 -0400

    changed body font to Rubik add Box shadow changed some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,28 @@ module.exports = {
   theme: {
     fontFamily: {
       title: ["Silkscreen", "cursive"],
-      body: ["Anton", "sans-serif"],
+      body: ["Rubik", "sans-serif"],
+    },
+    boxShadow: {
+      sm: "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+      DEFAULT: "2px 3px 1px 0px rgba(0, 0, 0, 0.4)",
+      md: "4px 6px 2px 0px rgb(0 0 0 / 0.2)",
+      lg: "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
+      xl: "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
+      "2xl": "0 25px 50px -12px rgb(0 0 0 / 0.25)",
+      inner: "inset 0 2px 4px 0 rgb(0 0 0 / 0.05)",
+      none: "none",
     },
     extend: {
       colors: {
         backgroud: "#292C35", // index backgroud
         "box-bg": "#353944",
         "box-bg-dark": "#2A2C32",
-        border: "#404658",
+        border: "#394159",
         "dot-line": "#404552",
         icon: "#4C535F", // icon color
-        primary: "#472BA2",
+        body: "#A3A3A3",
+        primary: "#5F22C4",
         "primary-light": "#5533C2",
         secondary: "#d1ec1a",
       },
