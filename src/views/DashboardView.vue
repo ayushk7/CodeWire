4cb7883bc105e72fc1cdbe9655d505904f633b4f
Løøt <66328711+L-o-o-t@users.noreply.github.com>
2022-08-18 12:53:12 -0400

    refactored the b to B add classes to main

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import Dashboard from '@/components/Dashboard.vue'
+import Dashboard from "@/components/DashBoard.vue";
 </script>
 
 <template>
-  <main>
+  <main class="w-full col-span-full">
     <Dashboard />
   </main>
 </template>
