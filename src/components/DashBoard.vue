dcfc0847593c9dec2ae6c575054247ea20e1e17b
Løøt <66328711+L-o-o-t@users.noreply.github.com>
2022-08-18 13:55:16 -0400

    Refactored for pretty

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -9,19 +9,20 @@ import DatabaseIcon from "./icons/IconDatabase.vue";
 <template>
   <h1 class="text-5xl mb-4">Dashboard</h1>
   <div
-    class="outline-dashed outline-2 outline-dot-line w-full h-16 p-4 mb-5 rounded-md text-3xl flex place-content-center">
+    class="outline-dashed outline-2 outline-dot-line w-full h-16 p-4 mb-5 rounded-md text-3xl flex place-content-center"
+  >
     <add-icon class="self-center text-icon" />
   </div>
-  <div class="shadow-md outline outline-2 outline-border bg-box-bg w-full p-4 rounded-md">
+  <div
+    class="shadow-md outline outline-2 outline-border bg-box-bg w-full p-4 rounded-md"
+  >
     <div class="flex justify-between mb-2 col-span-full row-span-full">
-      <h2 class=" text-base">Project Title</h2>
+      <h2 class="text-base">Project Title</h2>
       <div class="text-icon">
         <DotsIcon />
       </div>
     </div>
-    <div class="text-xs ">
-      Description
-    </div>
+    <div class="text-xs">Description</div>
     <div class="flex justify-between mt-4 col-span-full row-span-full">
       <div class="flex items-center">
         <FolderIcon class="text-icon h-3.5 w-3.5 mr-1.5" />
@@ -31,9 +32,7 @@ import DatabaseIcon from "./icons/IconDatabase.vue";
         <DatabaseIcon class="text-shadow text-icon h-3.5 w-3.5 mr-1.5" />
         <span class="text-xs text-icon">0/0</span>
       </div>
-      <div class="text-icon">
-        Updated 5 days ago
-      </div>
+      <div class="text-icon">Updated 5 days ago</div>
     </div>
   </div>
 </template>
