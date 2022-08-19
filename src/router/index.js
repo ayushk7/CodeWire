67211c58fb16b4d8097447b53a5591bca3084903
Løøt <66328711+L-o-o-t@users.noreply.github.com>
2022-08-19 15:32:00 -0400

    Start of making the DocumentationView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DashboardView from "../views/DashboardView.vue";
 import SupportView from "../views/SupportView.vue";
+import DocumentationView from "../views/DocumentationView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: "/",
       name: "dashboard",
       component: DashboardView,
+    },
+    {
+      path: "/documentation",
+      name: "documentation",
+      component: DocumentationView,
     },
     {
       path: "/support",
---
+++ src/views/DocumentationView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import DocumentationIcon from "../components/icons/IconDocumentation.vue";
+</script>
+<template>
+  <main class="w-full col-span-full">
+    <div class="flex items-center">
+      <DocumentationIcon class="w-7 h-7 mr-2 text-primary" />
+      <h1>Documentation</h1>
+    </div>
+    <div class="box p-5">
+      <div class="flex">
+        <div class="box-dark w-64">
+          <div class="text-body">Side Menu</div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style></style>
