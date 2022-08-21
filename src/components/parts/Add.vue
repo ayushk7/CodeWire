889a002fe59a1de684eb7bce9b92b78af563d95e
Løøt <66328711+L-o-o-t@users.noreply.github.com>
2022-08-21 14:05:34 -0400

    component for adding something

---
+++ src/components/parts/Add.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+import AddIcon from "../icons/IconAdd.vue";
+</script>
+<template>
+  <div class="flex items-baseline mb-12">
+    <AddIcon class="w-8 h-8 mr-2 text-primary" />
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
